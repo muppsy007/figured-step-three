6956bc6437f3ea053ef61647ea07a4367e856ae9
Aaron Cooper <aaron.cooper@solnet.co.nz>
2021-05-18 20:32:49 +1200

    Turnkey Setup - Slightly different approach for priming data for first start

--- init-app.sh
+++ init-app.sh
@@ -8,7 +8,11 @@ docker-compose exec app composer install
 docker-compose exec app yarn
 
 ## Run migrations to set up models to support functional test app
+docker-compose exec app php artisan migrate
+
+## Just for this test, migrate from scratch and seed
 docker-compose exec app php artisan migrate:fresh --seed
 
 ## Just do a migrate status so we can visually confirm all has gone well
 docker-compose exec app php artisan migrate:status
+
